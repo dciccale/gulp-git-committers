835219b70b222631762bc93daf5dcb63ae45c2b7
Denis Ciccale <dciccale@gmail.com>
2015-08-02 17:25:23 +0200

    init

--- README.md
+++ README.md
@@ -1,53 +1,31 @@
-(PLUGIN AUTHOR: Please read [Plugin README conventions](https://github.com/wearefractal/gulp/wiki/Plugin-README-Conventions), then delete this line)
-
 # gulp-git-committers
-[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url]  [![Coverage Status][coveralls-image]][coveralls-url] [![Dependency Status][depstat-image]][depstat-url]
 
-> git-committers plugin for [gulp](https://github.com/wearefractal/gulp)
+> Get a committer list from a git repository with some sorting and formatting options.
+
+## Install
+
+```sh
+$ npm install --save-dev gulp-git-committers
+```
 
 ## Usage
 
-First, install `gulp-git-committers` as a development dependency:
-
-```shell
-npm install --save-dev gulp-git-committers
-```
-
-Then, add it to your `gulpfile.js`:
-
 ```javascript
-var git-committers = require("gulp-git-committers");
+var gulp = require('gulp');
+var rev = require('gulp-git-committers');
 
-gulp.src("./src/*.ext")
-	.pipe(git-committers({
-		msg: "Hello Gulp!"
-	}))
-	.pipe(gulp.dest("./dist"));
+gulp.task('default', function () {
+  return committers()
+    .pipe(gulp.dest('./AUTHORS.txt'));
+});
 ```
 
-## API
+## Options
 
-### git-committers(options)
+For a full list of options visit the official `git-committers` documentation.
 
-#### options.msg
-Type: `String`  
-Default: `Hello World`
-
-The message you wish to attach to file.
-
+[git-committers documentation](https://github.com/dciccale/git-committers/blob/master/README.md#options)
 
 ## License
 
-[MIT License](http://en.wikipedia.org/wiki/MIT_License)
-
-[npm-url]: https://npmjs.org/package/gulp-git-committers
-[npm-image]: https://badge.fury.io/js/gulp-git-committers.png
-
-[travis-url]: http://travis-ci.org/dciccale/gulp-git-committers
-[travis-image]: https://secure.travis-ci.org/dciccale/gulp-git-committers.png?branch=master
-
-[coveralls-url]: https://coveralls.io/r/dciccale/gulp-git-committers
-[coveralls-image]: https://coveralls.io/repos/dciccale/gulp-git-committers/badge.png
-
-[depstat-url]: https://david-dm.org/dciccale/gulp-git-committers
-[depstat-image]: https://david-dm.org/dciccale/gulp-git-committers.png
+MIT: [http://denis.mit-license.org](http://denis.mit-license.org)
--- index.js
+++ index.js
@@ -1,104 +1,24 @@
-var through = require("through2");
-var gutil = require("gulp-util");
-var exec = require('child_process').exec;
+'use strict';
 var os = require('os');
-var _ = require('lodash');
+var through = require('through');
+var gutil = require('gulp-util');
+var gitCommitters = require('git-committers');
 
 module.exports = function (options) {
-  "use strict";
+  var stdout = '';
 
-  var defaults = {
-    sort: 'chronological', // Alphabetical, commits
-    email: false, // Show emails in the output
-    nomerges: false, // Only works when sorting by commits
-    output: './AUTHORS.txt' // The output file
-  };
-
-  options = defaults;
-
-
-  // see "Writing a plugin"
-  // https://github.com/gulpjs/gulp/blob/master/docs/writing-a-plugin/README.md
-  function gitCommitters(file, enc, callback) {
-    /*jshint validthis:true*/
-    var _format = function (stdout) {
-      var maxcol = 0;
-      var pad = ' ';
-
-      return stdout.replace(/^\s+|\s+$/g, '').split('\n').map(function (l) {
-        var numl = l.match(/\d+/);
-
-        if (numl) {
-          numl = numl[0].length;
-          maxcol = numl > maxcol ? numl : maxcol;
-          pad = '  ' + new Array(maxcol - numl + 1).join(' ');
-        }
-
-        return l.replace(/\t+/, pad).trim();
-      });
-    };
-
-    // Sort types
-    var sortMethod = {
-      alphabetical: 'sort',
-      chronological: 'reverse'
-    };
-
-    // Sort output
-    var _sort = function (stdout) {
-      if (sortMethod[options.sort]) {
-        stdout = _.unique(stdout[sortMethod[options.sort]]());
-      }
-      return stdout;
-    };
-
-    // Default command 'git'
-    var cmd = 'git';
-
-    if (options.sort === 'chronological' || options.sort === 'alphabetical') {
-      cmd += ' log --pretty="%aN';
-
-      // Show email
-      if (options.email) {
-        cmd += ' <%aE>';
-      }
-
-      cmd += '"';
-
-    // Sort by number of commits
-    } else if (options.sort === 'commits') {
-      cmd += ' shortlog -ns';
-
-      // Show email
-      if (options.email) {
-        cmd += 'e';
-      }
-
-      // Omit merge commits
-      if (options.nomerges) {
-        cmd += ' --no-merges';
-      }
-
-      // Detect current system use the appropriate terminal
-      if (/win32/.test(process.platform)) {
-        cmd += ' < CON';
-      } else {
-        cmd += ' < /dev/tty';
-      }
-    }
-
-    exec(cmd, function (error, stdout, stderr) {
-      if (!error) {
-        stdout = _format(stdout);
-        stdout = _sort(stdout);
-        grunt.file.write(options.output, stdout.join(os.EOL));
-        gutil.log('File "' + options.output + '" created.');
-        done();
-      } else {
-        gutil.log(error);
-      }
-    });
+  function write(data) {
+    stdout += data;
   }
 
-  return through.obj(gitCommitters);
+  function end() {
+    var file = new gutil.File({
+      path: './',
+      contents: new Buffer(stdout)
+    });
+    this.emit('data', file);
+    this.emit('end');
+  }
+
+  return gitCommitters(options, gutil.noop).stdout.pipe(through(write, end))
 };
